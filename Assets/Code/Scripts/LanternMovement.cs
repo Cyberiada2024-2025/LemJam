9dff577143b9ee317e9282c29283dc33ea462e48
Mikul-xyz <fmikulski@o2.pl>
2024-12-11 19:19:32 +0100

    Lantern state

--- Assets/Code/Scripts/LanternMovement.cs
+++ Assets/Code/Scripts/LanternMovement.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 
 public class LampionMovement : MonoBehaviour
 {
+    enum LampionState{
+        PRESPAWN,
+        RISING,
+        FLOATING
+    }
     private float time = 0.0f;
-
+    private LampionState state = LampionState.PRESPAWN;
 
     [SerializeField]
     AnimationCurve curve;
@@ -17,24 +22,41 @@ public class LampionMovement : MonoBehaviour
 
     [SerializeField]
     float max_time = 10;
-    
-    
-    
-    
- 
+
+
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         
+        //state = LampionState.RISING;
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        MoveLantern();
+
+        switch (state)
+        {
+            case LampionState.PRESPAWN:
+
+                break;
+            case LampionState.RISING:
+                MoveLantern();
+
+                break;
+            case LampionState.FLOATING:
+
+                break;
+        }
+
     }
+
 
 
     void MoveLantern()
@@ -42,6 +64,11 @@ public class LampionMovement : MonoBehaviour
         time += Time.deltaTime;
         float position = max_height * curve.Evaluate(time / max_time);
         transform.position = new Vector3(transform.position.x, position, transform.position.z);
+        if (time / max_time >= 1.0f)
+        {
+            this.state = LampionState.FLOATING;
+        }
+        return;
     }
 
 }
