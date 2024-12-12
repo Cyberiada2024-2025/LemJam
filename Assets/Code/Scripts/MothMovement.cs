164ea1147fa0fc3d046e7d9baabba6ffe41941cd
Jakub <kubawierzba1@wp.pl>
2024-12-12 10:25:36 +0100

    Moth speed and othersparams

--- Assets/Code/Scripts/MothMovement.cs
+++ Assets/Code/Scripts/MothMovement.cs
@@ -136,11 +136,11 @@ public class MothMovement : MonoBehaviour
                 }
             }
             
-            if (transform.position.y< DeathFallZone+ GameManager.Instance.cameraBox.GetY())
+            /*if (transform.position.y< DeathFallZone+ GameManager.Instance.cameraBox.GetY())
             {
                 source.PlayOneShot(DieFallSound, AudioListener.volume);
                 Death();
-            }
+            }*/
         }
 
         if (is_dead){
@@ -253,14 +253,20 @@ public class MothMovement : MonoBehaviour
 
     }
 
-    private void Death()
+    public void Death()
     {
         is_dead = true;
         StartCoroutine(Fade());
         ScoreTextSetter.scoreToSet = GameManager.Instance.GetScore(transform.position.z);
     }
-    
-private IEnumerator Fade(){
+
+    public void SafeDeath()
+    {        
+        StartCoroutine(Fade());
+        ScoreTextSetter.scoreToSet = GameManager.Instance.GetScore(transform.position.z);
+    }
+
+    private IEnumerator Fade(){
 		CanvasGroup canvasGroup = GameObject.Find("FadeToBlack").GetComponent<CanvasGroup>();
 		while (canvasGroup.alpha < 1){
 			canvasGroup.alpha += Time.deltaTime/3;
@@ -268,8 +274,7 @@ private IEnumerator Fade(){
 		}
         
          GameManager.Restart();
-		canvasGroup.interactable = false;
-		yield return null;
+		canvasGroup.interactable = false;	
     } 
     public void TurnOn()
     {
